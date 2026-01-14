48984a97123c4cbb74ffcf4ec9de74a4c9906ac7
kyf27@bath.ac.uk <kyf27@bath.ac.uk>
2026-01-14 22:21:25 +0000

    adding checkboxes

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const EXAMPLE_HABIT_TEXT = 'Example Habit Here';
 let habitList = document.getElementById('habit-list');
 let archivedListSn = document.getElementById('archived-list-section')
 
+const addCheckbox = (li) => {
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.className = "checkbox";
+  li.prepend(checkBox);
+};
+
 const getUserHabit = function () {
   let newHabit = prompt(`What is your new habit?`, 'To start meditating');
   if (!newHabit) {
@@ -20,6 +27,7 @@ const appendUserHabit = function () {
 
     const entry = document.createElement('li');
     entry.textContent = userHabit;
+    addCheckbox(entry);
 
     if (habitList.firstElementChild.textContent === EXAMPLE_HABIT_TEXT) {
       habitList.replaceChild(entry, habitList.firstElementChild);
