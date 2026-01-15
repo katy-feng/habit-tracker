e8b8b4dd472f860fd091dc398e788df9935e0ae1
kyf27@bath.ac.uk <kyf27@bath.ac.uk>
2026-01-15 19:46:26 +0000

    formatting

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const addHabitBtn = document.getElementById('add-habit-btn');
-const removeHabitBtn = document.getElementById('remove-habit-btn');
-const toggleBtn = document.getElementById('toggle-vis-btn');
+const addHabitBtn = document.getElementById("add-habit-btn");
+const removeHabitBtn = document.getElementById("remove-habit-btn");
+const toggleBtn = document.getElementById("toggle-vis-btn");
 
-const EXAMPLE_HABIT_TEXT = 'Example Habit Here';
+const EXAMPLE_HABIT_TEXT = "Example Habit Here";
 
-let habitList = document.getElementById('habit-list');
-let archivedListSn = document.getElementById('archived-list-section')
+let habitList = document.getElementById("habit-list");
+let archivedListSn = document.getElementById("archived-list-section");
 
 const addCheckbox = (li) => {
   const checkBox = document.createElement("input");
@@ -15,44 +15,42 @@ const addCheckbox = (li) => {
 };
 
 const getUserHabit = function () {
-  let newHabit = prompt(`What is your new habit?`, 'To start meditating');
+  let newHabit = prompt(`What is your new habit?`, "To start meditating");
   if (!newHabit) {
     return null;
   }
-  if (!newHabit.endsWith('.')) {
-    newHabit = newHabit + '.'
+  if (!newHabit.endsWith(".")) {
+    newHabit = newHabit + ".";
   }
   return newHabit;
 };
 
 const appendUserHabit = function () {
-    const userHabit = getUserHabit();
-    if (!userHabit) return;
+  const userHabit = getUserHabit();
+  if (!userHabit) return;
 
-    const entry = document.createElement('li');
-    entry.textContent = userHabit;
-    addCheckbox(entry);
+  const entry = document.createElement("li");
+  entry.textContent = userHabit;
+  addCheckbox(entry);
 
-    if (habitList.firstElementChild.textContent === EXAMPLE_HABIT_TEXT) {
-      habitList.replaceChild(entry, habitList.firstElementChild);
-    } else {
-      habitList.appendChild(entry);
-    }
-
-}
+  if (habitList.firstElementChild.textContent === EXAMPLE_HABIT_TEXT) {
+    habitList.replaceChild(entry, habitList.firstElementChild);
+  } else {
+    habitList.appendChild(entry);
+  }
+};
 
 const removeUserHabit = () => {
   lastChild = habitList.lastElementChild;
   habitList.removeChild(lastChild);
-}
+};
 
-addHabitBtn.addEventListener('click', appendUserHabit);
-removeHabitBtn.addEventListener('click', removeUserHabit);
+addHabitBtn.addEventListener("click", appendUserHabit);
+removeHabitBtn.addEventListener("click", removeUserHabit);
 
-toggleBtn.addEventListener('click', () => {
-  archivedListSn.classList.toggle('invisible');
-})
-
+toggleBtn.addEventListener("click", () => {
+  archivedListSn.classList.toggle("invisible");
+});
 
 // Notes
 // Toggle visibility button would be good to see past to-dos
